a12205d3711b02a6e4f9a094aaeaced9e4d7f014
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-04-11 02:21:02 +0530

    7 added addition.ts and related .js, .d.ts files

---
+++ src/arithmetic/addition.d.ts
@@ -0,0 +1,1 @@
+declare function addition(num1: number, num2: number): number;
---
+++ src/arithmetic/addition.ts
@@ -0,0 +1,27 @@
+/*
+    {
+        "created_at" : "11 March 2017 (before 02:05 am)",
+        "aim_of_script" : "To add 2 numbers",
+        "coded_by" : "Rishikesh Agrawani",
+    }
+*/
+
+function addition(num1:number, num2:number)
+{
+	return (num1 + num2)
+}
+
+console.log("Sum(12, 23) = " + addition(12, 23))
+
+console.log("Sum(23.67, 26.33) = " + addition(23.67, 26.33))
+
+console.log("Sum(23.67, 26.3) = " + addition(23.67, 26.3))
+
+console.log("Sum(237, 26.33) = " + addition(237, 26.33))
+
+/*
+Sum(12, 23) = 35
+Sum(23.67, 26.33) = 50
+Sum(23.67, 26.3) = 49.97
+Sum(237, 26.33) = 263.33
+*/
