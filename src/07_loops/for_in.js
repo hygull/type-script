993ee1deeb0ea58885dfb2b74519c942c4a3d014
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-04-19 23:53:05 +0530

    19 added a file related to the use of for/in loop in TypeScript

---
+++ src/07_loops/for_in.js
@@ -0,0 +1,12 @@
+/*
+    {
+        "created_at" : "19 April 2017",
+        "aim_of_script" : "Using for/in in TypeScript",
+        "coded_by" : "Rishikesh Agrawani",
+    }
+*/
+var arr = [12, 34, 56, 67, 89, 98.07, 67.67];
+var index;
+for (index in arr) {
+    console.log(arr[index]);
+}
