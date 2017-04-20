3115871334fd44937ba65a66895b05ef42fa06ba
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-04-20 08:15:33 +0530

    20 added a ts file related to the simple use of while loop

---
+++ src/07_loops/while.js
@@ -0,0 +1,13 @@
+/*
+    {
+        "created_at" : "19 April 2017",
+        "aim_of_script" : "Using while loop in TypeScript",
+        "coded_by" : "Rishikesh Agrawani",
+    }
+*/
+var arr = [67, 57, 83, 31, 97, 53];
+var i = 0;
+while (i < arr.length) {
+    console.log(arr[i]);
+    i += 1;
+}
---
+++ src/07_loops/while.ts
@@ -0,0 +1,16 @@
+/*
+    {
+        "created_at" : "19 April 2017",
+        "aim_of_script" : "Using while loop in TypeScript",
+        "coded_by" : "Rishikesh Agrawani",
+    }
+*/
+var arr:number[] = [67, 57, 83, 31, 97, 53]
+
+var i:number = 0
+
+while( i < arr.length )
+{
+	console.log( arr[i] )
+	i += 1
+} 
