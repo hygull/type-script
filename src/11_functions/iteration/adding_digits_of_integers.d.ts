4065f5cbb4ab4bdc9077744a8f548757f7a9aa2a
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-04-24 18:36:41 +0530

    46 adding digits of an integer

---
+++ src/11_functions/iteration/adding_digits_of_integers.d.ts
@@ -0,0 +1,1 @@
+declare function getSumOfDigits(n: number): number;
---
+++ src/11_functions/iteration/adding_digits_of_integers.js
@@ -0,0 +1,20 @@
+/*
+    {
+        "created_at" : "24 April 2017",
+        "aim_of_script" : "Adding digits of any +ve integer in TypeScript (using Iteration)",
+        "coded_by" : "Rishikesh Agrawani",
+    }
+*/
+function getSumOfDigits(n) {
+    var s = 0;
+    for (; n != 0; n = Math.floor(n / 10)) {
+        s += n % 10;
+    }
+    return s;
+}
+console.log("Sum of digits in integer 129324 is ", getSumOfDigits(129324));
+console.log("Sum of digits in integer 23476982 is ", getSumOfDigits(23476982));
+/*
+Sum of digits in integer 129324 is  21
+Sum of digits in integer 23476982 is  41
+*/ 
