94393481c83528c42eb1b1b062ddc85375394a89
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-05-02 23:50:10 +0530

    74 regular expression to match +ve integers like 123, 34 etc

---
+++ src/21_regexp/match_positive_integers.js
@@ -0,0 +1,27 @@
+/*
+    {
+        "created_at" : "2 May 2017",
+        "aim_of_script" : "Using regular expression in TypeScript",
+        "coded_by" : "Rishikesh Agrawani",
+    }
+*/
+//To match the positive integers like 123, 234, 1, 34 etc. 
+var intRegex = /^\d+$/;
+var s = "123";
+if (intRegex.test(s)) {
+    console.log(s + " is a +ve integer");
+}
+else {
+    console.log(s + " is not a +ve integer that matches /^\d+$/");
+}
+s = "-123";
+if (intRegex.test(s)) {
+    console.log(s + " is a +ve integer");
+}
+else {
+    console.log(s + " is not a +ve integer that matches /^\d+$/");
+}
+/*
+123 is a +ve integer
+-123 is not a +ve integer that matches /^d+$/
+*/ 
