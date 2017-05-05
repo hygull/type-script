b4d7eec6bddfd20f6024603757db8a69df680c23
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-05-05 09:03:33 +0530

    88 sort() method in TypeScript

---
+++ src/10_arrays/sort()_method.d.ts
@@ -0,0 +1,3 @@
+declare var arr: number[];
+declare var arr: number[];
+declare var cities: string[];
---
+++ src/10_arrays/sort()_method.ts
@@ -0,0 +1,47 @@
+/*
+    {
+        "created_at" : "4 May 2017",
+        "aim_of_script" : "Using sort() method",
+        "coded_by" : "Rishikesh Agrawani",
+    }
+*/
+
+var arr:number[] = [8, 12, 2, 34, 1, 56, 78, 7]
+console.log("Ascending order :-")
+console.log(arr)
+
+//Sorting the array in ascending order(integer array)
+arr.sort(function(a,b){
+	return a-b
+})
+
+console.log(arr)
+
+var arr:number[] = [8, 12, 2, 34, 1, 56, 78, 7]
+console.log("Descending order :-")
+console.log(arr)
+
+//Sorting the array in descending order(integer array)
+arr.sort(function(a,b){
+	return b-a
+})
+
+console.log(arr)
+
+//Sorting array of strings
+var cities:string[] = ["Bangalore","Dentolla" ,"Amarpur", "Colbia", "Bamgarh"]
+console.log(cities)
+
+cities.sort()
+console.log(cities)
+
+/*
+Ascending order :-
+[ 8, 12, 2, 34, 1, 56, 78, 7 ]
+[ 1, 2, 7, 8, 12, 34, 56, 78 ]
+Descending order :-
+[ 8, 12, 2, 34, 1, 56, 78, 7 ]
+[ 78, 56, 34, 12, 8, 7, 2, 1 ]
+[ 'Bangalore', 'Dentolla', 'Amarpur', 'Colbia', 'Bamgarh' ]
+[ 'Amarpur', 'Bamgarh', 'Bangalore', 'Colbia', 'Dentolla' ]
+*/
